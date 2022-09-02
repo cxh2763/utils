a0883514921d1acad3b5d6976c99eb6330a22d20
Cxh <276314445@qq.com>
2022-09-02 22:12:56 +0800

    feat:

--- test.js
+++ test.js
@@ -1,3 +1,173 @@
+// function solution(w, timeArr) {
+//   // 在这⾥写代码
+//   let result = ''
+//   if (timeArr.length <= w) {
+//     for (let i = 1; i <= w.length; i++) {
+//       result += i;
+//     }
+//     return result;
+//   }
+//   let Arr = new Array(w).fill(0).map(it => [0]);
+//   let time = 0;
+//   for (let i = 0; i < timeArr.length; i++) {
+//     for (let j = 0; j < w; j++) {
+//       if (Arr[j][Arr[j].length - 1] <= time) {
+//         result = result + (++j);
+//         Arr[j].push(timeArr[i])
+//       }
+//     }
+//     let timeArr = [];
+//     for (let j = 0; j < w; j++) {
+//       let nowtime = Arr[j].reduce((cur, pre) => {
+//         return cur + pre;
+//       }, 0);
+//       timeArr.push(nowtime);
+//     }
+//     timeArr.sort((a, b) => a - b);
+//     time = timeArr[0];
+//   }
+//   return result;
+// }
+
+// solution(3,)
+
+// function func(str) {
+//   let length = str.length;
+//   if (length % 2 !== 0) {
+//     return false;
+//   }
+//   let stack = [];
+//   let left = ['{', '[', '('];
+//   let right = ['}', ']', ')'];
+//   for (let i = 0; i < length; i++) {
+//     if (left.indexOf(str[i]) !== -1) {
+//       stack.push(str[i])
+//     } else {
+//       let index = right.indexOf(str[i]);
+//       if (left.indexOf(stack[stack.length - 1]) === index) {
+//         stack.pop();
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+//   if (stack.length === 0) {
+//     return true;
+//   }
+// }
+// func('{{[]}}')
+
+// var obj1 = { c: 1 }
+// var obj2 = { c: 1 }
+// obj1['a'] = obj2;
+// obj2['b'] = obj1;
+
+
+// function deepClone(param, map) {
+//   let result;
+//   if (typeof param !== 'object') {
+//     result = param;
+//   } else {
+//     //只考虑对象一种情况
+//     if (map.has(param)) {
+//       return param;
+//     }
+//     map.set(param, true);
+//     result = {};
+//     for (const key in param) {
+//       result[key] = deepClone(param[key], map);
+//     }
+//   }
+//   return result;
+// }
+// let newObj = deepClone(obj2, new Map());
+// console.log(newObj)
+
+// function solution(arr) {
+//   arr.sort((a, b) => {
+//     return a - b;
+//   })
+//   let max = 1;
+//   let ans = 1;
+//   let number = arr[0];
+//   let last = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (last === arr[i]) {
+//       ans++;
+//       if (ans > max) {
+//         max = ans;
+//         number = last;
+//       }
+//     } else {
+//       ans = 1;
+//       last = arr[i];
+//     }
+//   }
+//   console.log(number);
+//   return number;
+// }
+
+// function ListNode(val, next) {
+//   this.val = (val === undefined ? 0 : val)
+//   this.next = (next === undefined ? null : next)
+// }
+
+// let newHead = new ListNode(1);
+// let node = newHead;
+// for (let i = 2; i <= 5; i++) {
+//   node.next = new ListNode(i);
+//   node = node.next;
+// }
+// node.next = newHead;
+
+// var reverseList = function (head) {
+//   let pre = null;
+//   let cur = head;
+//   for (let i = 0; i < 5; i++) {
+//     let next = cur.next;
+//     cur.next = pre;
+//     pre = cur;
+//     cur = next;
+//   }
+//   return pre;
+// };
+
+// console.log(reverseList(newHead))
+
+// function solution(arr) {
+//   let s = new Map();
+//   let max = 0;
+//   for (const it of arr) {
+//     if (s.has(it)) {
+//       let now = s.get(it);
+//       s.set(it, ++now)
+//     } else {
+//       s.set(it, 1)
+//     }
+//   }
+//   console.log(s);
+//   for(it of s){
+//     console.log
+//   }
+//   s.forEach((item) => {
+//     if (item > max) {
+//       max = item;
+//     }
+//   })
+
+//   console.log(max);
+//   return max;
+// }
+
+// solution([3, 3, 3, 3, 3, 3, 3, 4, 1, 1, 1, 2, 2, 2])
+
+// console.log(a)
+// let a = 1;
+// var { foo: F, bar: B = "123" } = { foo: 'aaa', bar: 'bbb' };
+// console.log(F);
+// console.log(B);
+// console.log(foo);
+
 // const valueRaise = (item, k = 'value') => {
 //   for (const it in item) {
 //     if (typeof item[it] === 'object') {
--- utils/笔试题.js
+++ utils/笔试题.js
@@ -1,23 +1,139 @@
-function func(nodes) {
-  let result = [];
-  let path = [];
-  function findPath(nowNode) {
-    if (nowNode === nodes.length - 1) {
-      result.push(path.slice());
-      return;
-    }
-    for (let i = 0; i < nodes[nowNode].length; i++) {
-      path.push(nowNode)
-      findPath(nodes[nowNode][i]);
-      path.pop(nowNode);
-    }
-  }
-  findPath(0);
-  return result;
-}
-console.log(func([[1, 2, 3], [3], [3], [4], []]));
+
+// function func2(n) {
+//   let arr = new Array(n + 1).fill(0);
+//   arr[1] = 1;
+//   for (let i = 2; i <= n; i++) {
+//     arr[i] = i - 1 + arr[i - 1];
+//   }
+//   console.log(arr[n]);
+//   return arr[n];
+// }
+// func2(10000000)
 
 
+
+// class a {
+
+// }
+// console.log(typeof a);
+
+// var waysToStep = function (n) {
+//   let arr = new Array(n + 1).fill(0);
+//   arr[1] = 1;
+//   arr[2] = 2;
+//   arr[3] = 4;
+//   for (let i = 4; i <= n; i++) {
+//     arr[i] = (arr[i - 1] + arr[i - 2] + arr[i - 3]) % 1000000007;
+//   }
+//   return arr[n];
+// }
+// waysToStep(50)
+/**
+ *  1 2 3 4 5 6
+ *  2 1
+ *  3 2
+ *  4 2
+ *  5 3
+ *  6 3
+ *  1 -> 2
+ *  2 -> 1 3 4
+ *  3 -> 2 5 6
+ *  4 -> 2
+ *  5 -> 3
+ *  6 -> 3
+ */
+// function Node(value) {
+//   this.value = value;
+//   this.neighbor = [];
+// }
+// function creatGraph(n, arr) {
+//   let graph = [];
+//   for (let i = 0; i < n; i++) {
+//     graph[i] = new Node(i + 1);
+//   }
+//   let Arr = [];
+//   for (let i = 1; i <= arr.length; i++) {
+//     Arr.push([arr[i - 1], i + 1]);
+//     Arr.push([i + 1, arr[i - 1]]);
+//   }
+//   for (let i = 0; i < Arr.length; i++) {
+//     graph[Arr[i][0] - 1].neighbor.push(graph[Arr[i][1] - 1]);
+//   }
+//   return graph;
+// }
+// let graph = creatGraph(6, [1, 2, 2, 3, 3])
+
+// function deepSerach(node, graph, path) {
+//   if (path.indexOf(node) > -1) return;
+//   for (let i = 0; i < node.neighbor.length; i++) {
+//     if (path.indexOf(node.neighbor[i]) === -1 && path.indexOf(node) === -1) {
+//       path.push(node, node.neighbor[i])
+//     }
+//     deepSerach(node.neighbor[i], graph, path);
+//   }
+// }
+// let path = [];
+// for (let i = 0; i < graph.length; i++) {
+//   deepSerach(graph[i], graph, path);
+// }
+// console.log(path);
+// function func(n, k, arr) {
+//   let result = 0;
+//   for (let i = 0; i < n; i++) {
+//     for (let j = i + 1; j < n; j++) {
+//       if (arr[i] * arr[j] >= k) {
+//         result++;
+//       }
+//     }
+//   }
+//   result = result * 2;
+//   return result;
+// }
+// let arr = new Array(3000).fill(3)
+// console.log(func(3, 3, [3, 2, 1]))
+
+// //arr n * m
+// function func(n, m, id, arr) {
+//   let obj = {};
+//   let result = 1;
+//   for (let i = 0; i < n; i++) {
+//     obj[i + 1] = 0;
+//     for (let j = 0; j < m; j++) {
+//       obj[i + 1] += arr[i][j];
+//     }
+//   }
+//   let flag = obj[id];
+//   for (const it in obj) {
+//     if (obj[it] === flag) {
+//       if (+it < id) {
+//         result++
+//       }
+//     } else if (obj[it] > flag) {
+//       result++;
+//     }
+//   }
+//   console.log(result);
+//   return result;
+// }
+// func(4, 3, 2, [[90, 90, 90], [80, 100, 90], [80, 85, 85], [100, 100, 100]])
+// function func(nodes) {
+//   let result = [];
+//   let path = [];
+//   function findPath(nowNode) {
+//     if (nowNode === nodes.length - 1) {
+//       result.push(path.slice());
+//       return;
+//     }
+//     for (let i = 0; i < nodes[nowNode].length; i++) {
+//       path.push(nowNode)
+//       findPath(nodes[nowNode][i]);
+//       path.pop(nowNode);
+//     }
+//   }
+//   findPath(0);
+//   return result;
+// }
+// console.log(func([[1, 2, 3], [3], [3], [4], []]));
 
 // function func(n) {
 //   let result = 0;
